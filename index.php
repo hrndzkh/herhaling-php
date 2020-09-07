19dc6bbca7fe8b43a7b01c9eeb245b01a1e260cb
kit <2117920@talnet.nl>
2020-09-07 12:30:22 +0200

    oefeningen tijdens de les

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//variables
+//  $getal = 27;
+//  var_dump(is_int($getal));
+
+//array+for-loop
+//  $klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+//  print_r($klas);
+
+//  $klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+//  $length = count($klas);
+
+//  for ($i=0; $i < $length; $i++) {
+//    print $klas[$i].PHP_EOL;
+//  }
+//    $klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+
+//    foreach ($klas as $value) {
+//      echo $value.PHP_EOL;
+//    }
+
+?>
